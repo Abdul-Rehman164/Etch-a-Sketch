ee2ddf0708c3cf8cf4c31b3a61992923f92bf43a
Abdul Rehman <abdulrehmang571@gmail.com>
2023-03-05 19:01:23 +0500

    create the divs using js dom

---
+++ script.js
@@ -0,0 +1,14 @@
+const sketchPad = document.querySelector('.container');
+
+function getDivs(){
+    const NoOfGrids = Number(prompt('Enter a number less than 100'));
+    if (isNaN(NoOfGrids)) getDivs();
+    for (let i = 0;i < (NoOfGrids*NoOfGrids);i++){ //noOfGrids * noOfGrids one for row and one for column
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        sketchPad.appendChild(div);
+    }
+
+}
+
+getDivs();
