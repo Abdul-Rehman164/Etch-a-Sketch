9ac6e74a4ce72860a9cb983b823f58de22628c2d
Abdul Rehman <abdulrehmang571@gmail.com>
2023-03-06 15:07:20 +0500

    Added the pencil

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function getDivs(){
 
     for (let i = 0;i < (NoOfGrids*NoOfGrids);i++){ //noOfGrids * noOfGrids one for row and one for column
         const div = document.createElement('div');
-        div.classList.add('grid');
+        div.classList.add('grid','border');
         sketchPad.appendChild(div);
     }
     sketchPad.setAttribute('style',`grid-template-columns:repeat(${NoOfGrids},1fr); grid-template-rows:repeat(${NoOfGrids},1fr);`);
@@ -16,18 +16,29 @@ function getDivs(){
 }
 
 function getNumber(){
+    getDivs(); // for the default numbers
+    draw();
+    
     const NoOfGridsPara = document.querySelector('p');
     const rangeBar = document.querySelector('.range');
     rangeBar.addEventListener('change',() => {
         NoOfGrids = rangeBar.value; 
         getDivs();
+        draw();
     });
     rangeBar.addEventListener('input',() => {
         NoOfGridsPara.textContent = rangeBar.value + ' x ' + rangeBar.value;
     });
 }
 
+function draw(){
+    const boxes = document.querySelectorAll('.grid');
+    boxes.forEach(box => {
+        box.addEventListener('mouseover',() =>{
+            box.classList.add('draw');
+        });
+    })
+}
 
 let NoOfGrids = 20;
-getDivs() // for the default numbers
 getNumber();
